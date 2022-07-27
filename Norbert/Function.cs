4cd20878d6cf354ab8508019f802ee9f7ed34c13
Kevin White <kevin.white@clubpit.com>
2022-07-27 19:28:36 +0100

    Secrets implemented

--- Norbert/Function.cs
+++ Norbert/Function.cs
@@ -1,5 +1,9 @@
+using Amazon;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.LexV2Events;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+using System.Text.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -9,6 +13,10 @@ namespace Norbert;
 
 public class Function
 {
+    private static readonly RegionEndpoint primaryRegion = RegionEndpoint.EUWest2;
+    private static readonly String secretName = "ChatBot";
+    private static readonly String secretAlias = "AWSCURRENT";
+    //private Secrets? secrets = null;
 
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
@@ -18,6 +26,7 @@ public class Function
     /// <returns></returns>
     public LexV2Response FunctionHandler(LexEventV2 lexEvent, ILambdaContext context)
     {
+       
         IIntentProcessor process;
         IDictionary<String, String>? requestAttributes = null;
         IDictionary<String, String>? sessionAttributes = null;
@@ -25,17 +34,56 @@ public class Function
 
         try
         {
-            Console.WriteLine("------------");
-            Console.WriteLine("Bot           : " + lexEvent.Bot.Name);
-            Console.WriteLine("Alias         : " + lexEvent.Bot.AliasId);
-            Console.WriteLine("Version       : " + lexEvent.Bot.Version);
-            Console.WriteLine("Intent        :  " + lexEvent.Interpretations[0].Intent.Name);
-            Console.WriteLine("Transcription : " + lexEvent.InputTranscript);
-            Console.WriteLine("Source        : " + lexEvent.InvocationSource);
-            Console.WriteLine("Next Type     : " + lexEvent.ProposedNextState.DialogAction.Type);
-        }
-        catch(Exception) { }
+            Secrets secrets = GetSecrets().Result;
+            try
+            {
+                Console.WriteLine("------------");
+                Console.WriteLine("Secrets     : ");
+                Console.WriteLine("Secret URL    : " + secrets.qna_url_test);
+                Console.WriteLine("Bot           : " + lexEvent.Bot.Name);
+                Console.WriteLine("Alias         : " + lexEvent.Bot.AliasId);
+                Console.WriteLine("Version       : " + lexEvent.Bot.Version);
+                Console.WriteLine("Intent        :  " + lexEvent.Interpretations[0].Intent.Name);
+                Console.WriteLine("Transcription : " + lexEvent.InputTranscript);
+                Console.WriteLine("Source        : " + lexEvent.InvocationSource);
+                Console.WriteLine("Next Type     : " + lexEvent.ProposedNextState.DialogAction.Type);
+            }
+            catch (Exception) { }
 
+            IDictionary<String, String> requestAttributes = lexEvent.RequestAttributes ?? new Dictionary<String, String>();
+            IDictionary<String, String> sessionAttributes = lexEvent.SessionState.SessionAttributes ?? new Dictionary<String, String>();
+            IDictionary<String, LexV2.LexIntentV2.LexSlotV2> slots = lexEvent.Interpretations[0].Intent.Slots;
+            try
+            {
+                switch (lexEvent.Interpretations[0].Intent.Name.ToLower())
+                {
+                    case "debug":
+                        process = new DebugIntentProcessor();
+                        break;
+                    case "collectionday":
+                        process = new CollectionDayIntentProcessor();
+                        break;
+                    default:
+                        process = new DefaultIntentProcessor();
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                process = new DefaultIntentProcessor();
+            }
+            return process.Process(lexEvent, context, sessionAttributes, requestAttributes, slots);
+        }
+        catch(Exception error) 
+        {
+        }     
+    }
+
+    private static async Task<Secrets> GetSecrets()
+    {
+        IAmazonSecretsManager client = new AmazonSecretsManagerClient(primaryRegion);
+
+<<<<<<< Updated upstream
         try
         {
             requestAttributes = lexEvent.RequestAttributes ?? new Dictionary<String, String>();
@@ -43,26 +91,35 @@ public class Function
             slots = lexEvent.Interpretations[0].Intent.Slots;
         }
         catch (Exception) { }
+=======
+        GetSecretValueRequest request = new GetSecretValueRequest();
+        request.SecretId = secretName;
+        request.VersionStage = secretAlias;
+>>>>>>> Stashed changes
 
         try
         {
-            switch (lexEvent.Interpretations[0].Intent.Name.ToLower())
+            GetSecretValueResponse response = await client.GetSecretValueAsync(request);
+            Secrets? secrets = JsonSerializer.Deserialize<Secrets>(response.SecretString);
+            if(secrets is not null)
             {
-                case "debug":
-                    process = new DebugIntentProcessor();
-                    break;
-                case "collectionday":
-                    process = new CollectionDayIntentProcessor();
-                    break;
-                default:
-                    process = new DefaultIntentProcessor();
-                    break;
+                return secrets;
             }
-         }
-        catch(Exception)
-        {
-            process = new DefaultIntentProcessor();
+            else
+            {
+                throw new ApplicationException("Error Deserializing Secrets");
+            }
         }
-        return process.Process(lexEvent, context, sessionAttributes, requestAttributes, slots);
+        catch (Exception error)
+        {
+            throw new ApplicationException("Error Getting Secrets", error);
+        }
     }
+}
+public class Secrets
+{
+    public String? qna_auth_live { get; set; }
+    public String? qna_url_live { get; set; }
+    public String? qna_auth_test { get; set; }
+    public String? qna_url_test { get; set; }
 }
